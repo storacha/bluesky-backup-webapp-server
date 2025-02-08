7a25e72c0e0aff363cc9749d4fbff668e6362012
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2025-02-08 21:51:57 +0700

    Setup providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { BskyAuthProvider } from "@/contexts/bskyAuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { useState } from "react";
+
+export default function RootProviders({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BskyAuthProvider>{children}</BskyAuthProvider>
+    </QueryClientProvider>
+  );
+}
