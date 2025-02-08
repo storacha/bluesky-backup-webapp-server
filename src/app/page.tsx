d872324b2cb8ed4e8c2dd3bd9806e7bec5abc75f
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2025-02-08 22:15:18 +0700

    Add login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
-import Image from "next/image";
+"use client";
+
+import { useBskyAuthContext } from "@/contexts/bskyAuthProvider";
+import { bskyAuthClient } from "@/instances";
+import { useCallback, useState } from "react";
 
 export default function Home() {
+  const { authenticated } = useBskyAuthContext();
+
+  const [handle, setHandle] = useState<string>("");
+
+  const signIn = useCallback(async () => {
+    try {
+      await bskyAuthClient.signIn(handle, {
+        display: "popup",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [handle]);
+
   return (
     <>
-    <h1>Blusky Backup Webapp</h1>
-    <p>Lets get started</p>
+      <h1>Blusky Backup Webapp</h1>
+      <p>Lets get started</p>
+      <div>
+        <h4>Bluesky Auth</h4>
+        <div>{authenticated ? "Auth" : "Not auth"}</div>
+        <input
+          onChange={(e) => {
+            e.preventDefault();
+            setHandle(e.target.value);
+          }}
+          value={handle}
+          placeholder="Handler"
+        />
+        <button onClick={signIn}>Sign in</button>
+      </div>
     </>
   );
 }
