storacha/bluesky-backup-webapp

Format with Prettier

src/lib/backupMetadataStore.ts
@@ -1,13 +1,28 @@
-import db, { Backup, BackupsDB, Repo, Blob, PrefsDoc } from "./db"
+import db, { Backup, BackupsDB, Repo, Blob, PrefsDoc } from './db'
 
 interface BlobOptions {
   contentType?: string
 }
 
 export interface BackupMetadataStore {
-  addRepo: (cid: string, uploadCid: string, backupId: number, accountDid: string, commit: string) => Promise<void>
-  addPrefsDoc: (cid: string, backupId: number, accountDid: string) => Promise<void>
-  addBlob: (cid: string, backupId: number, accountDid: string, opts: BlobOptions) => Promise<void>
+  addRepo: (
+    cid: string,
+    uploadCid: string,
+    backupId: number,
+    accountDid: string,
+    commit: string
+  ) => Promise<void>
+  addPrefsDoc: (
+    cid: string,
+    backupId: number,
+    accountDid: string
+  ) => Promise<void>
+  addBlob: (
+    cid: string,
+    backupId: number,
+    accountDid: string,
+    opts: BlobOptions
+  ) => Promise<void>
   addBackup: (accountDid: string) => Promise<number>
   listBackups: () => Promise<Backup[]>
   getRepo: (backupId: number) => Promise<Repo | undefined>
@@ -18,30 +33,44 @@ export interface BackupMetadataStore {
 type BackupMetadataStoreInitializer = (db: BackupsDB) => BackupMetadataStore
 
 export const newBackupMetadataStore: BackupMetadataStoreInitializer = (db) => ({
-  async addRepo (cid, uploadCid, backupId, accountDid, commit) {
-    await db.repos.put({ cid, uploadCid, backupId, accountDid, commit, createdAt: new Date() })
+  async addRepo(cid, uploadCid, backupId, accountDid, commit) {
+    await db.repos.put({
+      cid,
+      uploadCid,
+      backupId,
+      accountDid,
+      commit,
+      createdAt: new Date(),
+    })
   },
-  async addPrefsDoc (cid, backupId, accountDid) {
+  async addPrefsDoc(cid, backupId, accountDid) {
     await db.prefsDocs.put({ cid, backupId, accountDid, createdAt: new Date() })
   },
-  async addBlob (cid, backupId, accountDid, { contentType } = {}) {
-    await db.blobs.put({ cid, contentType, backupId, accountDid, createdAt: new Date() })
+  async addBlob(cid, backupId, accountDid, { contentType } = {}) {
+    await db.blobs.put({
+      cid,
+      contentType,
+      backupId,
+      accountDid,
+      createdAt: new Date(),
+    })
   },
-  async addBackup (accountDid) {
+  async addBackup(accountDid) {
     return await db.backups.add({ accountDid, createdAt: new Date() })
   },
-  async listBackups () {
+  async listBackups() {
     return db.backups.toArray()
   },
-  async getRepo (backupId) {
+  async getRepo(backupId) {
     return db.repos.where('backupId').equals(backupId).first()
   },
-  async listBlobs (backupId) {
+  async listBlobs(backupId) {
     return db.blobs.where('backupId').equals(backupId).toArray()
   },
-  async getPrefsDoc (backupId) {
+  async getPrefsDoc(backupId) {
     return db.prefsDocs.where('backupId').equals(backupId).first()
-  }
+  },
 })
 
-export const backupMetadataStore: BackupMetadataStore = newBackupMetadataStore(db)
+export const backupMetadataStore: BackupMetadataStore =
+  newBackupMetadataStore(db)

src/lib/db.ts
@@ -3,63 +3,52 @@
 import Dexie, { type EntityTable } from 'dexie'
 
 export interface Backup {
-  id: number;
-  accountDid: string;
-  createdAt: Date;
+  id: number
+  accountDid: string
+  createdAt: Date
 }
 
 export interface Repo {
-  cid: string;
-  uploadCid: string;
-  backupId: number;
-  accountDid: string;
-  commit: string;
-  createdAt: Date;
-  encryptedWith?: string;
+  cid: string
+  uploadCid: string
+  backupId: number
+  accountDid: string
+  commit: string
+  createdAt: Date
+  encryptedWith?: string
 }
 export interface PrefsDoc {
-  cid: string;
-  backupId: number;
-  accountDid: string;
-  createdAt: Date;
-  encryptedWith?: string;
+  cid: string
+  backupId: number
+  accountDid: string
+  createdAt: Date
+  encryptedWith?: string
 }
 
 export interface Blob {
-  cid: string;
-  backupId: number;
-  accountDid: string;
-  contentType?: string;
-  createdAt: Date;
-  encryptedWith?: string;
+  cid: string
+  backupId: number
+  accountDid: string
+  contentType?: string
+  createdAt: Date
+  encryptedWith?: string
 }
 
 export type BackupsDB = Dexie & {
-  backups: EntityTable<
-    Backup,
-    'id'
-  >;
-  repos: EntityTable<
-    Repo,
-    'cid'
-  >;
-  prefsDocs: EntityTable<
-  PrefsDoc,
-  'cid'
->;
-  blobs: EntityTable<
-    Blob,
-    'cid'
-  >;
+  backups: EntityTable<Backup, 'id'>
+  repos: EntityTable<Repo, 'cid'>
+  prefsDocs: EntityTable<PrefsDoc, 'cid'>
+  blobs: EntityTable<Blob, 'cid'>
 }
 
-function newDB (name: string = 'storacha-bluesky-backups') {
+function newDB(name: string = 'storacha-bluesky-backups') {
   const db = new Dexie(name) as BackupsDB
 
   // Schema declaration:
   db.version(1).stores({
     backups: 'id++, accountDid, createdAt',
-    repos: 'cid, uploadCid, backupId, accountDid, commit, createdAt, encryptedWith',
+    repos:
+      'cid, uploadCid, backupId, accountDid, commit, createdAt, encryptedWith',
     prefsDocs: 'cid, backupId, accountDid, createdAt, encryptedWith',
     blobs: 'cid, contentType, backupId, accountDid, createdAt, encryptedWith',
   })

src/stories/Restore.stories.ts
@@ -1,19 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { RestoreDialogView } from '@/components/RestoreUI';
-import { fn } from '@storybook/test';
-import { CredentialSession } from '@atproto/api';
-
-
+import { RestoreDialogView } from '@/components/RestoreUI'
+import { fn } from '@storybook/test'
+import { CredentialSession } from '@atproto/api'
 
 const sourceSession = {
   did: 'did:key:sourceabc123xyz',
-  pdsUrl: new URL('https://bsky.social')
+  pdsUrl: new URL('https://bsky.social'),
 } as CredentialSession
 
 const sinkSession = {
   did: 'did:key:sinkabc123xyz',
-  pdsUrl: new URL('https://atproto.example.com')
+  pdsUrl: new URL('https://atproto.example.com'),
 } as CredentialSession
 
 const meta = {
@@ -32,43 +30,42 @@ const meta = {
     restorePrefsDoc: fn(),
     transferIdentity: fn(),
     sendPlcRestoreAuthorizationEmail: fn(),
-    setupPlcRestore: fn()
+    setupPlcRestore: fn(),
   },
-  decorators: [
-  ]
-} satisfies Meta<typeof RestoreDialogView>;
+  decorators: [],
+} satisfies Meta<typeof RestoreDialogView>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
-export const Initial: Story = {};
+export const Initial: Story = {}
 
 export const SourceAuthenticated: Story = {
   args: {
     sourceSession,
-  }
-};
+  },
+}
 
 export const SinkAuthenticated: Story = {
   args: {
     sourceSession,
     sinkSession,
-  }
-};
+  },
+}
 
 export const RecoveryConfirmationEmailSent: Story = {
   args: {
     sourceSession,
     sinkSession,
-    isPlcRestoreAuthorizationEmailSent: true
-  }
-};
+    isPlcRestoreAuthorizationEmailSent: true,
+  },
+}
 
 export const PlcRestoreSetup: Story = {
   args: {
     sourceSession,
     sinkSession,
     isPlcRestoreAuthorizationEmailSent: true,
-    isPlcRestoreSetup: true
-  }
-};
+    isPlcRestoreSetup: true,
+  },
+}

storacha/bluesky-backup-webapp-server

src/lib/backupMetadataStore.ts
@@ -1,4 +1,4 @@
-import db, { Backup, BackupsDB, Repo, Blob, PrefsDoc, KeyMeta } from "./db"
+import db, { Backup, BackupsDB, Repo, Blob, PrefsDoc, KeyMeta } from './db'
 
 interface EncryptableOptions {
   encryptedWith?: string
@@ -14,9 +14,25 @@ type BlobOptions = EncryptableOptions & {
 }
 
 export interface BackupMetadataStore {
-  addRepo: (cid: string, backupId: number, accountDid: string, commit: string, opts?: RepoOptions) => Promise<void>
-  addPrefsDoc: (cid: string, backupId: number, accountDid: string, opts?: PrefsDocOptions) => Promise<void>
-  addBlob: (cid: string, backupId: number, accountDid: string, opts?: BlobOptions) => Promise<void>
+  addRepo: (
+    cid: string,
+    backupId: number,
+    accountDid: string,
+    commit: string,
+    opts?: RepoOptions
+  ) => Promise<void>
+  addPrefsDoc: (
+    cid: string,
+    backupId: number,
+    accountDid: string,
+    opts?: PrefsDocOptions
+  ) => Promise<void>
+  addBlob: (
+    cid: string,
+    backupId: number,
+    accountDid: string,
+    opts?: BlobOptions
+  ) => Promise<void>
   addBackup: (accountDid: string) => Promise<number>
   addKey: (id: string, symkeyCid: string) => Promise<KeyMeta>
   listBackups: () => Promise<Backup[]>
@@ -30,41 +46,74 @@ export interface BackupMetadataStore {
 type BackupMetadataStoreInitializer = (db: BackupsDB) => BackupMetadataStore
 
 export const newBackupMetadataStore: BackupMetadataStoreInitializer = (db) => ({
-  async addRepo (cid, backupId, accountDid, commit, { encryptedWith, repoCid } = {}) {
-    await db.repos.put({ cid, repoCid, backupId, accountDid, commit, createdAt: new Date(), encryptedWith })
+  async addRepo(
+    cid,
+    backupId,
+    accountDid,
+    commit,
+    { encryptedWith, repoCid } = {}
+  ) {
+    await db.repos.put({
+      cid,
+      repoCid,
+      backupId,
+      accountDid,
+      commit,
+      createdAt: new Date(),
+      encryptedWith,
+    })
   },
-  async addPrefsDoc (cid, backupId, accountDid, { encryptedWith } = {}) {
-    await db.prefsDocs.put({ cid, backupId, accountDid, createdAt: new Date(), encryptedWith })
+  async addPrefsDoc(cid, backupId, accountDid, { encryptedWith } = {}) {
+    await db.prefsDocs.put({
+      cid,
+      backupId,
+      accountDid,
+      createdAt: new Date(),
+      encryptedWith,
+    })
   },
-  async addBlob (cid, backupId, accountDid, { contentType, encryptedWith } = {}) {
-    await db.blobs.put({ cid, contentType, backupId, accountDid, createdAt: new Date(), encryptedWith })
+  async addBlob(
+    cid,
+    backupId,
+    accountDid,
+    { contentType, encryptedWith } = {}
+  ) {
+    await db.blobs.put({
+      cid,
+      contentType,
+      backupId,
+      accountDid,
+      createdAt: new Date(),
+      encryptedWith,
+    })
   },
-  async addBackup (accountDid) {
+  async addBackup(accountDid) {
     return await db.backups.add({ accountDid, createdAt: new Date() })
   },
-  async addKey (id, symkeyCid) {
+  async addKey(id, symkeyCid) {
     const value = { id, symkeyCid }
     await db.keys.put(value)
     return value
   },
-  async listBackups () {
+  async listBackups() {
     return db.backups.toArray()
   },
-  async getRepo (backupId) {
+  async getRepo(backupId) {
     return db.repos.where('backupId').equals(backupId).first()
   },
-  async listBlobs (backupId) {
+  async listBlobs(backupId) {
     return db.blobs.where('backupId').equals(backupId).toArray()
   },
-  async getPrefsDoc (backupId) {
+  async getPrefsDoc(backupId) {
     return db.prefsDocs.where('backupId').equals(backupId).first()
   },
-  async listKeys () {
+  async listKeys() {
     return db.keys.toArray()
   },
-  async deleteKey (id) {
+  async deleteKey(id) {
     await db.keys.delete(id)
-  }
+  },
 })
 
-export const backupMetadataStore: BackupMetadataStore = newBackupMetadataStore(db)
+export const backupMetadataStore: BackupMetadataStore =
+  newBackupMetadataStore(db)

src/lib/db.ts
@@ -3,35 +3,35 @@
 import Dexie, { type EntityTable } from 'dexie'
 
 export interface Backup {
-  id: number;
-  accountDid: string;
-  createdAt: Date;
+  id: number
+  accountDid: string
+  createdAt: Date
 }
 
 export interface Repo {
-  cid: string;
-  repoCid?: string;
-  backupId: number;
-  accountDid: string;
-  commit: string;
-  createdAt: Date;
-  encryptedWith?: string;
+  cid: string
+  repoCid?: string
+  backupId: number
+  accountDid: string
+  commit: string
+  createdAt: Date
+  encryptedWith?: string
 }
 export interface PrefsDoc {
-  cid: string;
-  backupId: number;
-  accountDid: string;
-  createdAt: Date;
-  encryptedWith?: string;
+  cid: string
+  backupId: number
+  accountDid: string
+  createdAt: Date
+  encryptedWith?: string
 }
 
 export interface Blob {
-  cid: string;
-  backupId: number;
-  accountDid: string;
-  contentType?: string;
-  createdAt: Date;
-  encryptedWith?: string;
+  cid: string
+  backupId: number
+  accountDid: string
+  contentType?: string
+  createdAt: Date
+  encryptedWith?: string
 }
 
 export interface KeyMeta {
@@ -40,38 +40,24 @@ export interface KeyMeta {
 }
 
 export type BackupsDB = Dexie & {
-  backups: EntityTable<
-    Backup,
-    'id'
-  >;
-  repos: EntityTable<
-    Repo,
-    'cid'
-  >;
-  prefsDocs: EntityTable<
-    PrefsDoc,
-    'cid'
-  >;
-  blobs: EntityTable<
-    Blob,
-    'cid'
-  >;
-  keys: EntityTable<
-    KeyMeta,
-    'id'
-  >;
+  backups: EntityTable<Backup, 'id'>
+  repos: EntityTable<Repo, 'cid'>
+  prefsDocs: EntityTable<PrefsDoc, 'cid'>
+  blobs: EntityTable<Blob, 'cid'>
+  keys: EntityTable<KeyMeta, 'id'>
 }
 
-function newDB (name: string = 'storacha-bluesky-backups') {
+function newDB(name: string = 'storacha-bluesky-backups') {
   const db = new Dexie(name) as BackupsDB
 
   // Schema declaration:
   db.version(1).stores({
     backups: 'id++, accountDid, createdAt',
-    repos: 'cid, repoCid, backupId, accountDid, commit, createdAt, encryptedWith',
+    repos:
+      'cid, repoCid, backupId, accountDid, commit, createdAt, encryptedWith',
     prefsDocs: 'cid, backupId, accountDid, createdAt, encryptedWith',
     blobs: 'cid, contentType, backupId, accountDid, createdAt, encryptedWith',
-    keys: 'id, symkeyCid'
+    keys: 'id, symkeyCid',
   })
   return db
 }

src/stories/Restore.stories.ts
@@ -1,19 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { RestoreDialogView } from '@/components/RestoreUI';
-import { fn } from '@storybook/test';
-import { CredentialSession } from '@atproto/api';
-
-
+import { RestoreDialogView } from '@/components/RestoreUI'
+import { fn } from '@storybook/test'
+import { CredentialSession } from '@atproto/api'
 
 const sourceSession = {
   did: 'did:key:sourceabc123xyz',
-  serviceUrl: new URL('https://bsky.social')
+  serviceUrl: new URL('https://bsky.social'),
 } as CredentialSession
 
 const sinkSession = {
   did: 'did:key:sinkabc123xyz',
-  serviceUrl: new URL('https://atproto.example.com')
+  serviceUrl: new URL('https://atproto.example.com'),
 } as CredentialSession
 
 const meta = {
@@ -33,43 +31,42 @@ const meta = {
     restorePrefsDoc: fn(),
     transferIdentity: fn(),
     sendPlcRestoreAuthorizationEmail: fn(),
-    setupPlcRestore: fn()
+    setupPlcRestore: fn(),
   },
-  decorators: [
-  ]
-} satisfies Meta<typeof RestoreDialogView>;
+  decorators: [],
+} satisfies Meta<typeof RestoreDialogView>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
-export const Initial: Story = {};
+export const Initial: Story = {}
 
 export const SinkAuthenticated: Story = {
   args: {
     sinkSession,
-  }
-};
+  },
+}
 
 export const SourceAuthenticated: Story = {
   args: {
     sinkSession,
-    sourceSession
-  }
-};
+    sourceSession,
+  },
+}
 
 export const RecoveryConfirmationEmailSent: Story = {
   args: {
     sourceSession,
     sinkSession,
-    isPlcRestoreAuthorizationEmailSent: true
-  }
-};
+    isPlcRestoreAuthorizationEmailSent: true,
+  },
+}
 
 export const PlcRestoreSetup: Story = {
   args: {
     sourceSession,
     sinkSession,
     isPlcRestoreAuthorizationEmailSent: true,
-    isPlcRestoreSetup: true
-  }
-};
+    isPlcRestoreSetup: true,
+  },
+}