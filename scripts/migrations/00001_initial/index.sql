c3f15068257d5dab8bad4a123af5a2bea71196e1
Travis Vachon <travis.vachon@gmail.com>
2025-04-18 00:28:53 -0700

    feat: working backup config and backup creation!
    woo!

--- scripts/migrations/00001_initial/index.sql
+++ scripts/migrations/00001_initial/index.sql
@@ -1,23 +1,39 @@
-create table schedules (
-  id integer primary key generated always as identity,
-  account_did text not null,
-  name text,
-  bluesky_account text,
-  storacha_space text,
-  include_repository boolean,
-  include_blobs boolean,
-  include_preferences boolean,
-  created_at timestamp default current_timestamp,
-  updated_at timestamp default current_timestamp
-
+DROP TABLE IF EXISTS backup_configs CASCADE;
+CREATE TABLE IF NOT EXISTS backup_configs (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  account_did TEXT NOT NULL,
+  name TEXT NOT NULL,
+  bluesky_account TEXT NOT NULL,
+  storacha_space TEXT NOT NULL,
+  include_repository BOOLEAN NOT NULL,
+  include_blobs BOOLEAN NOT NULL,
+  include_preferences BOOLEAN NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-create table backups (
-  id integer primary key generated always as identity,
-  created_at timestamp default current_timestamp,
-  updated_at timestamp default current_timestamp,
-  schedule_id integer,
-  repository_cid text,
-  blobs_cid text,
-  preferences_cid text
+DROP TABLE IF EXISTS backups CASCADE;
+CREATE TABLE IF NOT EXISTS backups (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  backup_configs_id INTEGER NOT NULL,
+  repository_cid TEXT,
+  blobs_cid TEXT,
+  preferences_cid TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (backup_configs_id) REFERENCES backup_configs(id)
+);
+
+DROP TABLE IF EXISTS auth_sessions;
+CREATE TABLE IF NOT EXISTS auth_sessions (
+  key TEXT PRIMARY KEY,
+  value TEXT,
+  expiration_ttl INTEGER,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+DROP TABLE IF EXISTS auth_states;
+CREATE TABLE IF NOT EXISTS auth_states (
+  key TEXT PRIMARY KEY,
+  value TEXT,
+  expiration_ttl INTEGER,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
