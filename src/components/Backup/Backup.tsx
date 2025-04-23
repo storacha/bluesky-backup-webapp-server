172e1fa5c558b2256c9758609fc62c940d2aa753
kaf-lamed-beyt <belac335@gmail.com>
2025-04-23 17:49:59 +0100

    feat(ui): add modal component

--- src/components/Backup/Backup.tsx
+++ src/components/Backup/Backup.tsx
@@ -5,6 +5,8 @@ import { CaretDown, PlusCircle } from '@phosphor-icons/react'
 import { Property } from 'csstype'
 import { useState } from 'react'
 import { DataBox } from './Data'
+import { Modal } from '../ui/modal'
+import { useDisclosure } from '@/hooks/use-disclosure'
 
 interface BackupProps {
   id: number
@@ -55,7 +57,10 @@ const ConnectingLine = styled.div`
   margin: 0;
 `
 
-const AccountLogo = styled.div<{ $hasAccount?: boolean, $type: "original" | "grayscale" }>`
+const AccountLogo = styled.div<{
+  $hasAccount?: boolean
+  $type: 'original' | 'grayscale'
+}>`
   height: 42px;
   width: 42px;
   border-radius: 8px;
@@ -95,6 +100,7 @@ const DATA_BOXES: DataConfig[] = [
 ]
 
 export const Backup = ({ id, hasAccount }: BackupProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [data, setData] = useState<Record<string, boolean>>({
     repository: true,
     blobs: false,
@@ -109,75 +115,92 @@ export const Backup = ({ id, hasAccount }: BackupProps) => {
   }
 
   return (
-    <Container>
-      <Stack $gap="2rem">
-        <Heading>backup #{id}</Heading>
-        <Stack $gap="1rem">
-          <AccountsContainer>
-            <Box $background={hasAccount ? 'var(--color-white)' : ''}>
-              <Stack $gap=".8rem" $direction="row" $alignItems="center">
-                <AccountLogo $type='original' $hasAccount={hasAccount}>
-                  <Image
-                    src="/bluesky.png"
-                    alt="Bluesky Logo"
-                    width={25}
-                    height={25}
-                  />
-                </AccountLogo>
-                <Text>Add bluesky account</Text>
-              </Stack>
+    <>
+      <Container>
+        <Stack $gap="2rem">
+          <Heading>backup #{id}</Heading>
+          <Stack $gap="1rem">
+            <AccountsContainer>
+              <Box $background={hasAccount ? 'var(--color-white)' : ''}>
+                <Stack $gap=".8rem" $direction="row" $alignItems="center">
+                  <AccountLogo $type="original" $hasAccount={hasAccount}>
+                    <Image
+                      src="/bluesky.png"
+                      alt="Bluesky Logo"
+                      width={25}
+                      height={25}
+                    />
+                  </AccountLogo>
+                  <Text>Add bluesky account</Text>
+                </Stack>
+                <PlusCircle
+                  weight="fill"
+                  size="16"
+                  color="var(--color-gray-1)"
+                />
+              </Box>
+              <ConnectingLine />
+              <Box>
+                <Stack $gap=".8rem" $direction="row" $alignItems="center">
+                  <AccountLogo $type="grayscale" $hasAccount={hasAccount}>
+                    <Image
+                      src="/storacha-red.png"
+                      alt="Storacha Logo"
+                      width={18}
+                      height={18}
+                    />
+                  </AccountLogo>
+                  <Text>Select space</Text>
+                </Stack>
+                <CaretDown size="16" color="var(--color-gray-1)" />
+              </Box>
+            </AccountsContainer>
+          </Stack>
+
+          <Stack $gap="1.25rem" onClick={onOpen}>
+            <Text $textTransform="capitalize">keychain</Text>
+            <Box $height="44px" $width="48%">
+              <Text $textTransform="capitalize">create keychain</Text>
               <PlusCircle weight="fill" size="16" color="var(--color-gray-1)" />
             </Box>
-            <ConnectingLine />
-            <Box>
-              <Stack $gap=".8rem" $direction="row" $alignItems="center">
-                <AccountLogo $type='grayscale' $hasAccount={hasAccount}>
-                  <Image
-                    src="/storacha-red.png"
-                    alt="Storacha Logo"
-                    width={18}
-                    height={18}
-                  />
-                </AccountLogo>
-                <Text>Select space</Text>
-              </Stack>
-              <CaretDown size="16" color="var(--color-gray-1)" />
-            </Box>
-          </AccountsContainer>
-        </Stack>
-
-        <Stack $gap="1.25rem">
-          <Text $textTransform="capitalize">keychain</Text>
-          <Box $height="44px" $width="48%">
-            <Text $textTransform="capitalize">create keychain</Text>
-            <PlusCircle weight="fill" size="16" color="var(--color-gray-1)" />
-          </Box>
-        </Stack>
-        <Stack $gap="1.25rem">
-          <Text $textTransform="capitalize">data</Text>
-          <Stack $direction="row" $gap="1.25rem" $wrap="wrap">
-            {DATA_BOXES.map((box) => (
-              <DataBox
-                key={box.key}
-                title={box.title}
-                description={box.description}
-                value={data[box.key] || false}
-                onToggle={() => toggle(box.key)}
-              />
-            ))}
           </Stack>
+          <Stack $gap="1.25rem">
+            <Text $textTransform="capitalize">data</Text>
+            <Stack $direction="row" $gap="1.25rem" $wrap="wrap">
+              {DATA_BOXES.map((box) => (
+                <DataBox
+                  key={box.key}
+                  title={box.title}
+                  description={box.description}
+                  value={data[box.key] || false}
+                  onToggle={() => toggle(box.key)}
+                />
+              ))}
+            </Stack>
+          </Stack>
+          <Button
+            $background="var(--color-dark-blue)"
+            $color="var(--color-white)"
+            $textTransform="capitalize"
+            $width="fit-content"
+            $fontSize="0.75rem"
+            $mt="1.4rem"
+          >
+            create backup
+          </Button>
         </Stack>
-        <Button
-          $background="var(--color-dark-blue)"
-          $color="var(--color-white)"
-          $textTransform="capitalize"
-          $width="fit-content"
-          $fontSize="0.75rem"
-          $mt="1.4rem"
-        >
-          create backup
-        </Button>
-      </Stack>
-    </Container>
+      </Container>
+
+      <Modal
+        hasCloseBtn
+        title="Awesome Modal"
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <Box $borderStyle='none'>
+          <Text>Modal content goes here.</Text>
+        </Box>
+      </Modal>
+    </>
   )
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export * from './Button'
 export * from './Stack'
 export * from './center'
 export * from './text'
+export * from "./modal"
