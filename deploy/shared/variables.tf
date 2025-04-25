8e9789caa920f1348ea6b592bd01d77954404a71
hannahhoward <hannah@hannahhoward.net>
2025-04-25 13:42:46 -0700

    fix(deploy): fix unused variable for cloudflare

--- deploy/shared/variables.tf
+++ deploy/shared/variables.tf
@@ -8,11 +8,6 @@ variable "allowed_account_id" {
   type = string
 }
 
-variable "cloudflare_zone_id" {
-  description = "zone id for cloudflare dns records"
-  type        = string
-}
-
 variable "domain_base" {
   type = string
   default = ""
