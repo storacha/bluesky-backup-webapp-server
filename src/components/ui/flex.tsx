b84a25a512e49d581e9f80e569b3edec169c97c3
Petra Jaros <peeja@peeja.com>
2025-04-29 12:47:54 -0400

    Format 💅

--- src/components/ui/flex.tsx
+++ src/components/ui/flex.tsx
@@ -1,12 +1,12 @@
-import { styled } from "next-yak";
-import { StyleProps } from "./style";
+import { styled } from 'next-yak'
+import { StyleProps } from './style'
 
 const Flex = styled.div<Partial<StyleProps>>`
   height: ${({ $height = '66px' }) => $height};
   width: ${({ $width = '100%' }) => $width};
   display: flex;
   justify-content: space-between;
-  align-items: ${({$alignItems = ''} ) => $alignItems};
+  align-items: ${({ $alignItems = '' }) => $alignItems};
   padding: ${({ $padding = '0 0.6rem' }) => $padding};
   gap: ${({ $gap = 0 }) => $gap};
   cursor: pointer;
