b3857f0000d975ee08405e1039898e58adcc9cf8
Petra Jaros <peeja@peeja.com>
2025-05-06 12:17:54 -0400

    Add back `console.error(err)` removed by mistake

--- src/app/sentry.tsx
+++ src/app/sentry.tsx
@@ -6,5 +6,6 @@ import { captureException } from '@sentry/nextjs'
  * @param err the error - typed as unknown to match catch(err)
  */
 export function logAndCaptureError(err: unknown) {
+  console.error(err)
   captureException(err)
 }
