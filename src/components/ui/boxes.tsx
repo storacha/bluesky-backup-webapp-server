f4fefd730b3927e92d13a608ddc16a4bf5a90d56
Petra Jaros <peeja@peeja.com>
2025-05-06 17:21:38 -0400

    Fix CSS parsing issue (in VS Code, at least)

--- src/components/ui/boxes.tsx
+++ src/components/ui/boxes.tsx
@@ -15,12 +15,12 @@ export const Container = styled.div`
 `
 
 export const Box = styled.div<Partial<StyleProps & { $isFocused?: boolean }>>`
-  border: ${({ $borderWidth = '1px', $isFocused }) =>
-      $isFocused ? '2px' : $borderWidth}
-    ${({ $borderStyle = 'dashed', $isFocused }) =>
-      $isFocused ? 'solid' : $borderStyle}
-    ${({ $borderColor = 'var(--color-gray-light)', $isFocused }) =>
-      $isFocused ? 'var(--color-dark-blue)' : $borderColor};
+  border-width: ${({ $borderWidth = '1px', $isFocused }) =>
+    $isFocused ? '2px' : $borderWidth};
+  border-style: ${({ $borderStyle = 'dashed', $isFocused }) =>
+    $isFocused ? 'solid' : $borderStyle};
+  border-color: ${({ $borderColor = 'var(--color-gray-light)', $isFocused }) =>
+    $isFocused ? 'var(--color-dark-blue)' : $borderColor};
   border-radius: 12px;
   height: ${({ $height = '66px' }) => $height};
   width: ${({ $width = '100%' }) => $width};
