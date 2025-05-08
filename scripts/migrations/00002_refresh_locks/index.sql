393f92c487391f2b1654ddfc81cba721a4a224ac
Travis Vachon <travis.vachon@gmail.com>
2025-05-08 10:20:02 -0700

    fix: clean up refresh_locks table definition

--- scripts/migrations/00002_refresh_locks/index.sql
+++ scripts/migrations/00002_refresh_locks/index.sql
@@ -2,7 +2,5 @@
 
 CREATE TABLE IF NOT EXISTS refresh_locks (
   key TEXT PRIMARY KEY,
-  value TEXT,
-  expiration_ttl INTEGER,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+  value TEXT
 );
