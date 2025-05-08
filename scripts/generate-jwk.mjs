aa410b34a5da14cac5d438e5eb9464ca7790ad1e
Travis Vachon <travis.vachon@gmail.com>
2025-05-08 11:32:27 -0700

    fix: format

--- scripts/generate-jwk.mjs
+++ scripts/generate-jwk.mjs
@@ -2,5 +2,5 @@ import { JoseKey } from '@atproto/jwk-jose'
 import * as uuid from 'uuid'
 const keypair = await JoseKey.generate(['ES256'], uuid.v4())
 
-consolr.log("this can be used as the value of TOKEN_ENDPOINT_PRIVATE_KEY_JWK:")
+console.log('this can be used as the value of TOKEN_ENDPOINT_PRIVATE_KEY_JWK:')
 console.log(JSON.stringify(keypair.privateJwk))
