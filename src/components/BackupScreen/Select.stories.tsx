dc613a13b83cff78af9b58b2751bea8bf1e7d4f3
Petra Jaros <peeja@peeja.com>
2025-05-12 13:20:36 -0400

    Reimplement `Select` (with unstyled options)

---
+++ src/components/BackupScreen/Select.stories.tsx
@@ -0,0 +1,73 @@
+import { userEvent, within } from '@storybook/test'
+
+import { Select } from './Select'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Select> = {
+  title: 'Components/Select',
+  component: Select,
+  parameters: {
+    parentSize: {
+      width: '300px',
+    },
+    backgrounds: {
+      values: [
+        {
+          name: 'Backup',
+          value: 'var(--color-light-blue-10)',
+        },
+      ],
+      default: 'Backup',
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/Lbzwo4ulF1yjgYEITUz3vV/Storacha?node-id=417-102715',
+    },
+  },
+  args: {
+    name: 'atproto_account',
+    imageSrc: '/bluesky.png',
+    label: 'Bluesky account',
+    items: [
+      {
+        id: 'did:plc:ro3eio7zgqosf5gnxsq6ik5m',
+        label: 'chalametoui.bsky.social',
+      },
+    ],
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const NoValue: Story = {}
+
+export const NoValueHover: Story = {
+  parameters: { pseudo: { hover: true } },
+}
+
+export const NoValueFocus: Story = {
+  play: async () => {
+    // Focus the button (including :focus-visible)
+    await userEvent.tab()
+  },
+}
+
+export const ValueSelected: Story = {
+  play: async ({ canvasElement }) => {
+    // Use the entire body to find things in portals outside the canvas element.
+    const body = within(canvasElement.ownerDocument.body)
+    const button = await body.findByRole('button', {
+      name: 'Select Bluesky account',
+    })
+    await userEvent.click(button)
+    const listBox = await body.findByRole('listbox', {
+      name: 'Select Bluesky account',
+    })
+    const option = await within(listBox).findByRole('option', {
+      name: 'chalametoui.bsky.social',
+    })
+    await userEvent.click(option)
+  },
+}
