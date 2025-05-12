25f92b4017821a4c9dffa62a572b4cdd51b5f4ba
Travis Vachon <travis.vachon@gmail.com>
2025-05-12 12:15:25 -0700

    fix: add dynamic = 'force-dynamic' to green the build

--- src/app/identities/page.tsx
+++ src/app/identities/page.tsx
@@ -4,6 +4,9 @@ import { getSession } from '@/lib/sessions'
 
 import IdentitiesPage from './IdentitiesPage'
 
+// we need this here because of the call to findAuthedBskyAccounts which uses secrets only available at runtime
+export const dynamic = 'force-dynamic'
+
 export default async function Identities() {
   const { did: account } = await getSession()
   const { authSessionStore } = getStorageContext()
