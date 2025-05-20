6f900957f5bba09bf9b066a29c14e659fed91362
Caleb! <48127194+kaf-lamed-beyt@users.noreply.github.com>
2025-05-20 15:54:06 +0100

    Update src/lib/server/db.ts
    Co-authored-by: Petra Jaros <peeja@peeja.com>

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -333,7 +333,7 @@ export function getStorageContext(): StorageContext {
 
         return {
           count: count,
-          results: results || [],
+          results: results,
         }
       },
       async addBackup(input) {
