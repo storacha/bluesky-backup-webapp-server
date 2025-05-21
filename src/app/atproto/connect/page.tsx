fbffd137ea30bfef3db876fe709ceb3709519413
Travis Vachon <travis@storacha.network>
2025-05-21 16:08:12 -0700

    fix: polish up the bluesky account connection flow (#181)
    1) use a server action to enable us to show progress (I tried a few
    other ways to no avail)
    2) move handle inference to the server action - the quick little handle
    update didn't look great and the user sees the full handle on the next
    screen
    3) add error handling - we were just throwing an error if a user, for
    instance, entered a nonexistent handle. now we show the error and let
    them reset the form, per the [next.js
    docs](https://nextjs.org/docs/app/api-reference/file-conventions/error)

--- src/app/atproto/connect/page.tsx
+++ src/app/atproto/connect/page.tsx
@@ -1,35 +1,40 @@
 'use client'
-import { styled } from 'next-yak'
+import Form from 'next/form'
 import React from 'react'
+import { useFormStatus } from 'react-dom'
 
 import {
-  Button,
   Center,
   Heading,
   InputField,
   Stack,
+  StatefulButton,
   SubHeading,
 } from '@/components/ui'
 import { useStorachaAccount } from '@/hooks/use-plan'
 
-const ConnectStack = styled(Stack)`
-  padding: 0 2rem;
-`
+import connectHandle from './connectHandle'
+import { ConnectStack } from './ui'
+
+const ConnectButton = () => {
+  const { pending } = useFormStatus()
+  return (
+    <StatefulButton
+      isLoading={pending}
+      disabled={pending}
+      $background="var(--color-dark-blue)"
+      $height="fit-content"
+      $fontSize="0.75rem"
+      type="submit"
+    >
+      Connect
+    </StatefulButton>
+  )
+}
 
 const ConnectPage: React.FC = () => {
   const account = useStorachaAccount()
   if (!account) return null
-
-  const handleAppend = (e: React.FormEvent<HTMLFormElement>) => {
-    const handleInput = e.currentTarget.elements.namedItem(
-      'handle'
-    ) as HTMLInputElement
-    const handle = handleInput.value.trim()
-
-    if (!handle.includes('.')) {
-      handleInput.value = `${handle}.bsky.social`
-    }
-  }
 
   return (
     <Center $height="100vh">
@@ -40,7 +45,7 @@ const ConnectPage: React.FC = () => {
             To get started, please log in to your Bluesky account.
           </SubHeading>
         </Stack>
-        <form action="/atproto/oauth" method="POST" onSubmit={handleAppend}>
+        <Form action={connectHandle}>
           <input type="hidden" name="account" value={account.did()} />
           <Stack $gap="1rem">
             <InputField
@@ -49,16 +54,9 @@ const ConnectPage: React.FC = () => {
               label="Bluesky Handle"
               placeholder="Enter your handle"
             />
-            <Button
-              $background="var(--color-dark-blue)"
-              $height="fit-content"
-              $fontSize="0.75rem"
-              type="submit"
-            >
-              Connect
-            </Button>
+            <ConnectButton />
           </Stack>
-        </form>
+        </Form>
       </ConnectStack>
     </Center>
   )
