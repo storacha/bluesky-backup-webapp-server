de3aef0cc940efd4676fd15d116962f194f5d72b
ash <alan138@gmail.com>
2025-05-29 17:07:21 +0100

    feat: add x-client header (#193)
    This allows us to distinguish invocations made by this app vs others.
    Allows us to track the relative success of this application.
    
    
    ---------
    
    Co-authored-by: Travis Vachon <travis.vachon@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import { withYak } from 'next-yak/withYak'
+import { version } from './package.json'
 
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  env: {
+    version,
+  },
   output: 'standalone',
   images: {
     remotePatterns: [
