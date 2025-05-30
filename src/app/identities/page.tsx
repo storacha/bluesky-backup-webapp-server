83ef1f8aed2e9bb52d158c035d73adec8f7704f4
Caleb! <48127194+kaf-lamed-beyt@users.noreply.github.com>
2025-05-30 23:53:33 +0100

    feat: add 'connect new account' button to identities page (#194)
    closes [#443](https://github.com/storacha/project-tracking/issues/443)

--- src/app/identities/page.tsx
+++ src/app/identities/page.tsx
@@ -1,8 +1,6 @@
 import { findAllIdentities } from '@/lib/atproto'
 import { getSession } from '@/lib/sessions'
 
-import { AppLayout } from '../AppLayout'
-
 import IdentitiesPage from './IdentitiesPage'
 
 // we need this here because of the call to findAuthedBskyAccounts which uses secrets only available at runtime
@@ -12,9 +10,5 @@ export default async function Identities() {
   await getSession() // Keep session check but don't use the result
   const identities = await findAllIdentities()
 
-  return (
-    <AppLayout selectedBackupId={null}>
-      <IdentitiesPage identities={identities} />
-    </AppLayout>
-  )
+  return <IdentitiesPage identities={identities} />
 }
