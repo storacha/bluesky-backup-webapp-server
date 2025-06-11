6cadfad9c93471b092c0cd1ad0704684bc9b650b
Travis Vachon <travis.vachon@gmail.com>
2025-06-11 12:41:17 -0700

    chore: factor constants into constants

--- src/components/HumanodeAuthLink.tsx
+++ src/components/HumanodeAuthLink.tsx
@@ -5,6 +5,12 @@ import { base64url } from 'multiformats/bases/base64'
 import { styled } from 'next-yak'
 import { useEffect, useState } from 'react'
 
+import {
+  HUMANODE_AUTH_URL,
+  HUMANODE_CLIENT_ID,
+  HUMANODE_OAUTH_CALLBACK_URL,
+} from '@/lib/constants'
+
 const HumanodeLink = styled.a`
   background-color: var(--color-dark-blue);
   color: var(--color-white);
@@ -52,7 +58,7 @@ export default function HumanodeAuthLink() {
     },
     [client, account]
   )
-  const humanodeHref = `${process.env.NEXT_PUBLIC_HUMANODE_AUTH_URL}?response_type=code&client_id=${process.env.NEXT_PUBLIC_HUMANODE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_HUMANODE_OAUTH_CALLBACK_URL}&scope=openid&state=${state}`
+  const humanodeHref = `${HUMANODE_AUTH_URL}?response_type=code&client_id=${HUMANODE_CLIENT_ID}&redirect_uri=${HUMANODE_OAUTH_CALLBACK_URL}&scope=openid&state=${state}`
   return (
     <HumanodeLink href={humanodeHref} target="_blank" rel="noopener noreferrer">
       Give me a free plan!
