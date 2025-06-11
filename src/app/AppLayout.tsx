649a53acff0dfee14868a62e863a30ea351215a0
kaf-lamed-beyt <belac335@gmail.com>
2025-06-12 00:33:46 +0100

    format

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -18,15 +18,9 @@ const LayoutContainer = styled.div`
 const MainContent = styled.main`
   flex: 1;
   min-width: 0;
-<<<<<<< HEAD
-  display: flex;
-  height: 100vh;
-  overflow-y: auto;
-=======
   height: 100vh;
   overflow: auto;
   background-color: var(--color-light-blue-10);
->>>>>>> 7d4a449ad609e341032e4b0334dfda35038902b3
 `
 
 export const AppLayout = ({ children, selectedBackupId }: AppLayoutProps) => {
