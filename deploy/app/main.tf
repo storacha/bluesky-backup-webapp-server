436be82a841c5334fab471ae325b0bdcf401dccb
Hannah Howard <hannah@hannahhoward.net>
2025-06-11 17:39:43 -0700

    fix(deploy): remove staging replication (#220)
    # Goals
    
    Fix staging deploys
    
    # Implementation
    
    Bluesky and telegram were over writing each other unfortunately for the
    staging replication, which turns out to be a singleton-per-region
    object. so it's configured manually for now.
    
    update storoku, which leaves replication to be configed manually

--- deploy/app/main.tf
+++ deploy/app/main.tf
@@ -52,7 +52,7 @@ resource "random_password" "session_password" {
 
 
 module "app" {
-  source = "github.com/storacha/storoku//app?ref=v0.2.40"
+  source = "github.com/storacha/storoku//app?ref=v0.2.41"
   private_key = var.private_key
   private_key_env_var = "SERVER_IDENTITY_PRIVATE_KEY"
   httpport = 3000
