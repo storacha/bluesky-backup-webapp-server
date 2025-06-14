ad0ddf2b643f825ccffb46281369968ede9b3490
luna <luna@wvvw.me>
2025-06-14 16:31:51 +0100

    fix: dynamically import createSnapshot

--- src/app/backups/[id]/CreateSnapshotButton.tsx
+++ src/app/backups/[id]/CreateSnapshotButton.tsx
@@ -1,35 +1,38 @@
 'use client'
 
 import { useAuthenticator } from '@storacha/ui-react'
+import { useEffect, useState } from 'react'
 
 import { CreateButton } from '@/components/ui/CreateButton'
 import { delegate } from '@/lib/delegate'
 import { useSWRMutation } from '@/lib/swr'
 import { Backup } from '@/types'
 
-let createSnapshot: typeof import('./createSnapshot').createSnapshot
-
-if (process.env.STORYBOOK) {
-  createSnapshot = () => {
-    throw new Error('Server Functions are not available in Storybook')
-  }
-} else {
-  createSnapshot = (await import('./createSnapshot')).createSnapshot
-}
-
 export const CreateSnapshotButton = ({ backup }: { backup: Backup }) => {
   const [{ accounts, client }] = useAuthenticator()
   const account = accounts[0]
+  const [createSnapshot, setCreateSnapshot] = useState<
+    typeof import('./createSnapshot').createSnapshot | null
+  >(null)
+
+  useEffect(() => {
+    if (process.env.STORYBOOK) {
+      return
+    }
+
+    const importCreateSnapshot = async () => {
+      const { createSnapshot } = await import('./createSnapshot')
+      setCreateSnapshot(createSnapshot)
+    }
+    importCreateSnapshot()
+  }, [])
 
   const enabled = client && backup
   const { trigger, isMutating } = useSWRMutation(
     enabled && ['api', `/api/backups/${backup.id}/snapshots`],
     async () => {
-      // SWR guarantees this won't actually happen.
-      if (!enabled)
-        throw new Error('Mutation ran but key should have been null')
-      const delegationData = await delegate(client, backup.storachaSpace)
-      await createSnapshot({ backupId: backup.id, delegationData })
+      const delegationData = await delegate(client!, backup.storachaSpace)
+      await createSnapshot?.({ backupId: backup.id, delegationData })
     }
   )
 
