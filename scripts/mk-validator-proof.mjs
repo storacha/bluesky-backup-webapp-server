3c0ca4a75a754d858c0c56e49be11fd76b4a8211
Alan Shaw <alan@storacha.network>
2025-06-27 13:34:05 +0100

    chore: appease linter

--- scripts/mk-validator-proof.mjs
+++ scripts/mk-validator-proof.mjs
@@ -1,7 +1,7 @@
 /**
  * Create a "ucan/attest" delegation allowing UCANs that have attestions issued
  * by "alias-did" to be validated.
- * 
+ *
  * Usage: node mk-validator-proof.mjs <service-did-web> <service-private-key> <alias-did-web>
  */
 import * as DID from '@ipld/dag-ucan/did'
@@ -11,14 +11,16 @@ import { base64 } from 'multiformats/bases/base64'
 import { identity } from 'multiformats/hashes/identity'
 import * as Link from 'multiformats/link'
 
-const service = ed25519.parse(process.argv[3]).withDID(DID.parse(process.argv[2]).did())
+const service = ed25519
+  .parse(process.argv[3])
+  .withDID(DID.parse(process.argv[2]).did())
 const alias = DID.parse(process.argv[4])
 
 const delegation = await delegate({
   issuer: service,
   audience: alias,
   capabilities: [{ can: 'ucan/attest', with: service.did() }],
-  expiration: Infinity
+  expiration: Infinity,
 })
 
 const res = await delegation.archive()
